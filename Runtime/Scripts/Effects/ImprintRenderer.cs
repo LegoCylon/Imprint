206b2bc7e08b6b7549c430347445f777de51bfbd
LegoCylon <4934232+LegoCylon@users.noreply.github.com>
2020-11-08 20:19:53 -0800

    - Cleaned up ImprintRenderer.

--- Runtime/Scripts/Effects/ImprintRenderer.cs
+++ Runtime/Scripts/Effects/ImprintRenderer.cs
@@ -33,6 +33,8 @@ namespace Imprint.Runtime.Effects
 
         public override void Render(PostProcessRenderContext context)
         {
+            // Ensure that the camera is setup to manage a depth texture so that it can be used by the Imprint/Make
+            // shader.
             if ((context.camera.depthTextureMode & DepthTextureMode.Depth) != DepthTextureMode.Depth)
             {
                 context.camera.depthTextureMode |= DepthTextureMode.Depth;
@@ -41,40 +43,41 @@ namespace Imprint.Runtime.Effects
             var sheet = context.propertySheets.Get(shader: Shader.Find(name: "Imprint/Show"));
             sheet.properties.SetFloat(name: "_Blend", value: settings.Blend);
 
+            // Draw Imprint renderers into a temporary render texture (using the Imprint/Make shader) so that the Show
+            // shader knows where they all are.
             context.command.BeginSample(name: "Imprint");
             context.GetScreenSpaceTemporaryRT(cmd: context.command, nameID: sRenderTextureID);
             context.command.SetRenderTarget(rt: sRenderTextureID);
             context.command.ClearRenderTarget(clearDepth: false, clearColor: true, backgroundColor: Color.black);
 
+            // Note that for simplicity this doesn't do any culling or instancing.
             foreach (ImprintBehaviour imprintBehaviour in ImprintBehaviour.Instances)
             {
-                switch (imprintBehaviour.Renderer)
+                if (imprintBehaviour.Renderer == null)
                 {
-                    case null:
-                        break;
-                    default:
-                        Bounds bounds = imprintBehaviour.Renderer.bounds;
-                        float width = Mathf.Max(a: bounds.size.x, b: bounds.size.y);
-                        width = Mathf.Max(a: width, b: bounds.size.z);
-                        Vector3 scale = Vector3.one * width;
-                        Matrix4x4 matrix = Matrix4x4.TRS(
-                            pos: bounds.center,
-                            q: imprintBehaviour.Renderer.transform.rotation,
-                            s: scale);
-                        context.command.DrawMesh(mesh: _Mesh, matrix: matrix, material: _Material);
-                        break;
+                    continue;
                 }
+
+                // Create a uniformly scaled shape using the max bounds along any axis to ensure it keeps a consistent
+                // size no matter how the character rotates.
+                Bounds bounds = imprintBehaviour.Renderer.bounds;
+                float width = Mathf.Max(a: bounds.size.x, b: bounds.size.y);
+                width = Mathf.Max(a: width, b: bounds.size.z);
+                Vector3 scale = Vector3.one * width;
+                Matrix4x4 matrix = Matrix4x4.TRS(
+                    pos: bounds.center,
+                    q: imprintBehaviour.Renderer.transform.rotation,
+                    s: scale);
+                context.command.DrawMesh(mesh: _Mesh, matrix: matrix, material: _Material);
             }
 
-            #if true
+            // Now that all the imprints have been made into the temporary render texture, perform postprocessing using
+            // the Show shader.
             context.command.SetGlobalTexture(nameID: sRenderTextureID, value: sRenderTextureID);
             context.command.BlitFullscreenTriangle(source: context.source,
                 destination: context.destination,
                 propertySheet: sheet,
                 pass: 0);
-            #else
-            context.command.CopyTexture(src: sRenderTextureID, dst: context.destination);
-            #endif
 
             context.command.ReleaseTemporaryRT(nameID: sRenderTextureID);
             context.command.EndSample(name: "Imprint");
